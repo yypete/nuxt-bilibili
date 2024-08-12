c99bd8888fd4f7586f79b11e346efc91514e8d13
Peter <Peteryyp007@gmail.com>
2024-08-12 23:39:07 +0800

    feat：文件路由

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- 首页路由 -->
+    <nuxt-link to="/">首页</nuxt-link>
+    <!-- 附页路由 -->
+    <nuxt-link to="/video">附页</nuxt-link>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/video/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>附页</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

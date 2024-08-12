8ca172a7cd6fa0f7a0af52e54b535138ccff62a5
Peter <Peteryyp007@gmail.com>
2024-08-13 00:24:17 +0800

    feat:vant组件库的使用

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 useSeoMeta({
   title: "哔哩哔哩 (゜-゜)つロ 干杯~-bilibili",
-  description:'哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。',
-  keywords:"Bilibili,哔哩哔哩,哔哩哔哩动画,哔哩哔哩弹幕网,弹幕视频,B站,弹幕,字幕,AMV,MAD,MTV,ANIME,动漫,动漫音乐,游戏,游戏解说,二次元,游戏视频"
+  description:
+    "哔哩哔哩(bilibili.com)是国内知名的视频弹幕网站,这里有及时的动漫新番,活跃的ACG氛围,有创意的Up主。大家可以在这里找到许多欢乐。",
+  keywords:
+    "Bilibili,哔哩哔哩,哔哩哔哩动画,哔哩哔哩弹幕网,弹幕视频,B站,弹幕,字幕,AMV,MAD,MTV,ANIME,动漫,动漫音乐,游戏,游戏解说,二次元,游戏视频",
 });
 </script>
 
@@ -13,5 +15,16 @@ useSeoMeta({
     <!-- 附页路由 -->
     <nuxt-link to="/video">附页</nuxt-link>
     <NuxtPage />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
   </div>
 </template>
+
+<style>
+:root {
+  --van-primary-color: #fb7299 !important;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  // 开启调试工具
+  devtools: { enabled: true },
+  // 应用组件库模块
+  modules: ["@vant/nuxt"],
+});
